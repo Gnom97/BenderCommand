63e8ed5d7cf2b057597a1918fbdd2f685dbf4402
Gnom97 <thomas.zerr@googlemail.com>
2017-11-22 16:23:06 +0100

    Kommando erlaubt live manuelle Steuerung

--- BenderCommand/ModeManCommand.py
+++ BenderCommand/ModeManCommand.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 from Command import command
+import keyboard
+
+forward = 'w'
+backward = 's'
+left = 'a'
+right = 'd'
+stop = 'esc'
 
 def modeMan(connection):
 	answer = command(connection, b'man\n', None)
@@ -10,3 +17,20 @@ def modeMan(connection):
 		print("Manueller Modus aktiviert")
 	else:
 		print("Manueller Modus wurde nicht aktiviert")
+
+	while True:
+		inputStr = ''
+
+		if keyboard.is_pressed(forward):
+			inputStr += forward
+		if keyboard.is_pressed(backward):
+			inputStr += backward
+		if keyboard.is_pressed(right):
+			inputStr += right
+		if keyboard.is_pressed(left):
+			inputStr += left
+		if keyboard_is_pressed(stop):
+			break
+
+		inputStr += '\n'
+		command(connection, inputStr.encode("UTF-8"), None)
