3b89b36e6e24037ccf5a8e2326639005f716de1b
Kevin Kaufmann <Kaufmann.Kevin@outlook.de>
2017-11-28 12:24:37 +0100

    exit und help Kommando hinzugefügt

---
+++ BenderCommand/HelpCommand.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+def helpFunc(connection):
+	print("Kommandos:")
+	print("modeauto\tWechseln in den Automatischen Modus")
+	print("modeman\tWechseln in den Manuellen Modus")
+	print("exit\tBeenden des Programmes")
