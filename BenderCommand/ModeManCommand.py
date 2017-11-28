b5913644935a999487b8909394b620087c4a96cd
Kevin Kaufmann <Kaufmann.Kevin@outlook.de>
2017-11-28 12:00:19 +0100

    WASD-Steurung gefixt

--- BenderCommand/ModeManCommand.py
+++ BenderCommand/ModeManCommand.py
@@ -8,6 +8,16 @@ left = 'a'
 right = 'd'
 stop = 'esc'
 
+forwardNewline = 'w\n'
+backwardNewline = 's\n'
+leftNewline = 'a\n'
+rightNewline = 'd\n'
+
+forward_byte = forwardNewline.encode("UTF-8")
+backward_byte = backwardNewline.encode("UTF-8")
+left_byte = leftNewline.encode("UTF-8")
+right_byte = rightNewline.encode("UTF-8")
+
 def modeMan(connection):
 	answer = command(connection, b'man\n', None)
 	answer = int(answer)
@@ -19,18 +29,20 @@ def modeMan(connection):
 		print("Manueller Modus wurde nicht aktiviert")
 
 	while True:
-		inputStr = ''
-
 		if keyboard.is_pressed(forward):
-			inputStr += forward
-		if keyboard.is_pressed(backward):
-			inputStr += backward
-		if keyboard.is_pressed(right):
-			inputStr += right
-		if keyboard.is_pressed(left):
-			inputStr += left
-		if keyboard.is_pressed(stop):
+			command(connection, forward_byte, 0)
+			print("gesendet w")
+		elif keyboard.is_pressed(backward):
+			command(connection, backward_byte, 0)
+			print("gesendet s")
+		elif keyboard.is_pressed(right):
+			command(connection, right_byte, 0)
+			print("gesendet d")
+		elif keyboard.is_pressed(left):
+			command(connection, left_byte, 0)
+			print("gesendet a")
+		elif keyboard.is_pressed(stop):
+			print("break")
 			break
-
-		inputStr += '\n'
-		command(connection, inputStr.encode("UTF-8"), None)
+		else:
+			command(connection, b'0\n', 0)
