2d172218cda9fa570093a9b36e92b42b8baf70a1
Unknown <thomas.zerr@googlemail.com>
2017-11-30 17:42:38 +0100

    Server Klasse hinzugefügt

--- BenderCommand/server/server.py
+++ BenderCommand/server/server.py
@@ -1,2 +1,19 @@
 # -*- coding: utf-8 -*-
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
+PORT = 80
+SERVER_ADDRESS = ("", PORT)
+
+class Server(BaseHTTPRequestHandler):
+    def __init__(self, commandsdict):
+        #Dictionary aller Kommandos
+        self.commandsdict = commandsdict
+        #HTTPServer
+        self.http_server = HTTPServer(SERVER_ADDRESS, Server)
+
+    def run(self):
+        print("HTTP Server wird gestartet")
+        self.http_server.serve_forever()
+
+    def do_GET(self):
+        pass
